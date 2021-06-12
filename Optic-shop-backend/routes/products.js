040bd9f1d37def6224b1925f55605d4052e38c69
ghadakhedri1 <61046136+ghadakhedri1@users.noreply.github.com>
2021-06-12 19:22:43 +0100

    CRUD product

--- Optic-shop-backend/routes/products.js
+++ Optic-shop-backend/routes/products.js
@@ -41,4 +41,70 @@ router.get("/", async (req, res) => {
   }
 });
 
+//@route PUT products/:id
+//@desc update product
+//@access Private
+router.put(
+  "/:id",async (req, res) => {
+    try {
+      const newProduct = {
+        name: req.body.name,
+        description: req.body.description,
+        //   image: req.body.image,
+        //   images: req.body.images,
+        //   brand: req.body.brand,
+        //   price: req.body.price,
+        //   category: req.body.category,
+        //   rating: req.body.rating,
+        //   isFeatured: req.body.isFeatured,
+        //   countInSock: req.body.countInSock,
+        //   status: req.body.status,
+      };
+
+      let product = await Product.findOne({ _id: req.params.id });
+
+      if (!product) {
+        return res.status(404).json({ message: "Product not Found " });
+      }
+
+      if (product) {
+        product = await Product.findOneAndUpdate(
+          { _id: req.params.id },
+          { $set: newProduct },
+          { new: true }
+        );
+        return res.json(product);
+      }
+
+      await product.save();
+      res.json(product);
+    } catch (error) {
+      console.error(error.message);
+      if (error.kind === "ObjectId") {
+        return res.status(404).json({ message: "Product not Found " });
+      }
+      res.status(500).send("Server error");
+    }
+  }
+);
+//@route DELETE api/products/:id
+//@desc DELETE by id product
+//@access Private
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    await product.remove();
+    res.json({ message: "Product Deleted" });
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind === "ObjectId") {
+      return res.status(404).json({ message: "Post not Found " });
+    }
+    res.status(500).send("Server error");
+  }
+});
+
+
+
 module.exports = router;
